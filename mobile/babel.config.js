344241490f7bc9dc2616d023a90ce2c2a4a1f4f0
José Ronaldo de Oliveira Júnior <joseronaldobpg@gmail.com>
2022-10-14 02:39:24 -0300

    ⚰️ remove mobile

--- mobile/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    "module:metro-react-native-babel-preset",
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-  ],
-};
--- mobile/jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
-};
