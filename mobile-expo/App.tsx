81f69976b75ced34a2c8aa82040b3276f4df6287
José Ronaldo de Oliveira Júnior <joseronaldobpg@gmail.com>
2022-10-17 00:43:25 -0300

    ⚰️ removing test code

--- mobile-expo/App.tsx
+++ mobile-expo/App.tsx
@@ -8,16 +8,18 @@ import {
   Poppins_500Medium,
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
-import { UserRepository } from "../core/src";
 
 export default function App() {
-  const something = new UserRepository();
-  something.getUser();
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_700Bold,
   });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <SignIn></SignIn>
