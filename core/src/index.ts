f6c85b0957ac6f84e486c77d6a4f38cd78a5de92
José Ronaldo de Oliveira Júnior <joseronaldobpg@gmail.com>
2022-12-12 16:13:36 -0300

    DOTENV

--- core/src/index.ts
+++ core/src/index.ts
@@ -97,13 +97,16 @@
 //create a express app example
 
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log("Example app listening on port 3000!");
 });
